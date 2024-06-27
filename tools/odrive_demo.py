3b43e13f8d2f80325bec5b607a655487f697159e
Andrew-y21 <121197362+Andrew-y21@users.noreply.github.com>
2024-06-27 11:53:55 -0400

    Update odrive_demo.py

--- tools/odrive_demo.py
+++ tools/odrive_demo.py
@@ -10,41 +10,134 @@ from odrive.enums import *
 import time
 import math
 
+
+
+wheelRadius = 0.2286
+DimBetweenWheels = 0.762
 # Find a connected ODrive (this will block until you connect one)
 print("finding an odrive...")
-my_drive = odrive.find_any()
+odrv0 = odrive.find_any()
 
 # Calibrate motor and wait for it to finish
 print("starting calibration...")
-my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while odrv0.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while odrv0.axis1.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
-my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+velo = 0
+angle = 0
+def inputt():
+    while True:
+        c = input("command:")
+        if c == 'w':
+            return 1
+        elif c == 's':
+            return 2
+        elif c == 'a':
+            return 3
+        elif c == 'd':
+            return 4
+        else:
+            return 0
+        
+def drive(velo, angle):
+    vL= (2*velo-angle*DimBetweenWheels)/2*wheelRadius
+    vR= -(2*velo+angle*DimBetweenWheels)/2*wheelRadius
+
+    odrv0.axis0.controller.input_vel = vL
+    odrv0.axis1.controller.input_vel = vR
+
+
+
+def command_to_drive(command, velo, angle):
+        
+        if command == 1:
+            velo += rate_of_increase
+            
+            return angle, velo
+        elif command == 2:
+            velo -= rate_of_increase
+            
+            return angle, velo
+        elif command == 3:
+            angle -= rate_of_increase
+            
+            return angle, velo
+        elif command == 4:
+            angle += rate_of_increase
+            return angle, velo
+
+
+
+        
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 # To read a value, simply read the property
-print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
+print("Bus voltage is " + str(odrv0.vbus_voltage) + "V")
+
+
+#velo = input('velo')
+#velo = float(velo)
+
+#angle = input('angle')
+#angle = float(angle)
+
+#vL= (2*velo-angle*DimBetweenWheels)/2*wheelRadius
+#vR= -(2*velo+angle*DimBetweenWheels)/2*wheelRadius
+
+#odrv0.axis0.controller.input_vel = vL
+#odrv0.axis1.controller.input_vel = vR
+
+rate_of_increase = 5
+help = 1
+
+while help == 1:
+    command = inputt()
+    if command == 0:
+        help = 0
+        angle = 0
+        velo = 0
+        drive(velo, angle)
+        break
+    angle, velo = command_to_drive(command, velo, angle)
+    print(angle, velo)
+    drive(velo, angle)
+
+
+
+        
+
+
+    
+        
+
+
+    
 
 # Or to change a value, just assign to the property
-my_drive.axis0.controller.input_pos = 3.14
-print("Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
+#odrv0.axis0.controller.input_pos = 3.14
+#print("Position setpoint is " + str(odrv0.axis0.controller.pos_setpoint))
 
 # And this is how function calls are done:
-for i in [1,2,3,4]:
-    print('voltage on GPIO{} is {} Volt'.format(i, my_drive.get_adc_voltage(i)))
+#for i in [1,2,3,4]:
+#    print('voltage on GPIO{} is {} Volt'.format(i, odrv0.get_adc_voltage(i)))
 
 # A sine wave to test
-t0 = time.monotonic()
-while True:
-    setpoint = 4.0 * math.sin((time.monotonic() - t0)*2)
-    print("goto " + str(int(setpoint)))
-    my_drive.axis0.controller.input_pos = setpoint
-    time.sleep(0.01)
+#t0 = time.monotonic()
+#while True:
+#    setpoint = 4.0 * math.sin((time.monotonic() - t0)*2)
+#    print("goto " + str(int(setpoint)))
+#    odrv0.axis0.controller.input_pos = setpoint
+#    time.sleep(0.01)
 
 # Some more things you can try:
 
 # Write to a read-only property:
-my_drive.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
+#odrv0.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
 
 # Assign an incompatible value:
-my_drive.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
+#odrv0.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
